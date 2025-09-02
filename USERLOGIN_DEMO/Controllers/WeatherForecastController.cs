fbadd385e748d555d2800c7b7586db930a92eb1a
gnanasekaranvelusamyp <gnanasekaranvelusamy.p@gmail.com>
2025-09-02 06:48:29 +0530

    last changes

--- USERLOGIN_DEMO/Controllers/WeatherForecastController.cs
+++ USERLOGIN_DEMO/Controllers/WeatherForecastController.cs
@@ -29,5 +29,16 @@ namespace USERLOGIN_DEMO.Controllers
             })
             .ToArray();
         }
+
+        public IEnumerable<WeatherForecast> Update() 
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
