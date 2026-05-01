9399f423d7095488fafd4456a0f9561307fd7341
gnanasekaranvelusamyp <gnanasekaranvelusamy.p@gmail.com>
2026-05-01 15:34:30 +0530

    customer details add

---
+++ USERLOGIN_DEMO/Controllers/CustomerController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using USERLOGIN_DEMO.Model;
+
+namespace USERLOGIN_DEMO.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly string connectionString =
+             "Host=aws-1-ap-south-1.pooler.supabase.com;Port=6543;Database=postgres;Username=postgres.eyaoebwnpupwpeucamki;Password=Sekar@1996##;SSL Mode=Require;Trust Server Certificate=true;";
+
+        [HttpPost]
+        public IActionResult AddCustomer([FromBody] Customer customer)
+        {
+            using NpgsqlConnection conn = new NpgsqlConnection(connectionString);
+            conn.Open();
+
+            string query = @"INSERT INTO customers
+            (customer_name, contact_person, mobile_no, email, gst_no, address, city, state, pincode)
+            VALUES
+            (@customer_name, @contact_person, @mobile_no, @email, @gst_no, @address, @city, @state, @pincode)";
+
+            using NpgsqlCommand cmd = new NpgsqlCommand(query, conn);
+
+            cmd.Parameters.AddWithValue("@customer_name", customer.customer_name);
+            cmd.Parameters.AddWithValue("@contact_person", (object?)customer.contact_person ?? DBNull.Value);
+            cmd.Parameters.AddWithValue("@mobile_no", (object?)customer.mobile_no ?? DBNull.Value);
+            cmd.Parameters.AddWithValue("@email", (object?)customer.email ?? DBNull.Value);
+            cmd.Parameters.AddWithValue("@gst_no", (object?)customer.gst_no ?? DBNull.Value);
+            cmd.Parameters.AddWithValue("@address", (object?)customer.address ?? DBNull.Value);
+            cmd.Parameters.AddWithValue("@city", (object?)customer.city ?? DBNull.Value);
+            cmd.Parameters.AddWithValue("@state", (object?)customer.state ?? DBNull.Value);
+            cmd.Parameters.AddWithValue("@pincode", (object?)customer.pincode ?? DBNull.Value);
+
+            cmd.ExecuteNonQuery();
+
+            return Ok(new { message = "Customer Added Successfully" });
+        }
+    }
+}
---
+++ USERLOGIN_DEMO/Model/Customer.cs
@@ -0,0 +1,25 @@
+﻿namespace USERLOGIN_DEMO.Model
+{
+    public class Customer
+    {
+        public long customer_id { get; set; }
+
+        public string customer_name { get; set; }
+
+        public string? contact_person { get; set; }
+
+        public string? mobile_no { get; set; }
+
+        public string? email { get; set; }
+
+        public string? gst_no { get; set; }
+
+        public string? address { get; set; }
+
+        public string? city { get; set; }
+
+        public string? state { get; set; }
+
+        public string? pincode { get; set; }
+    }
+}
